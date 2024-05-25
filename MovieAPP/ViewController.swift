65e65ccd88fec0b58197d2a9467abeb0771a03c2
Michael Rodriguez <miarodriguezur@unal.edu.co>
2024-05-25 08:36:53 -0500

    Initial Commit

---
+++ MovieAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAPP
+//
+//  Created by Michael Alexander Rodriguez Urbina on 25/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
